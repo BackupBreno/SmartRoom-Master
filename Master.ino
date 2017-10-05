1caa499ddc1b5e9ae9943075e43baf4040d4efc1
Breno Aquino <brenoaquinop7@gmail.com>
2017-10-05 00:10:38 -0300

    Adicionado requisições para manipulação do Roteador e do Modem.

--- Master.ino
+++ Master.ino
@@ -5,53 +5,86 @@
 #include "Configuracoes.h"
 #include "Pinos.h"
 
-// Reqesuts
+// Light Requests
 
-void postLedStatusVerde(char * params = "")
+void postLight(char * params = "")
 {
 
-	LOG_PRINTLN("POST: Setar ou Resetar LED Verde");
+	LOG_PRINTLN("POST: Ligar ou Desligar a Luz.");
 
 	if (strncmp(params, "active=0", 8) == 0)
-	{
+		lightWith_0();
 
-		statusLedSystemVerde = 0;
-
-		digitalWrite(ledSystemVerde, LOW);
-
-		LOG_PRINTLN("Led Verde: OFF");
-
-	}
 	else if (strncmp(params, "active=1", 8) == 0)
-	{
+		lightWith_1();
 
-		statusLedSystemVerde = 1;
-
-		digitalWrite(ledSystemVerde, HIGH);
-
-		LOG_PRINTLN("Led Verde: ON");
-	}
-
-	rest.addData("active", statusLedSystemVerde);
+	rest.addData("active", lightState);
 }
 
-void getLedStatusVerde(char * params = "") {
+void getLight(char * params = "")
+{
+	LOG_PRINTLN("GET: Verificar Estado da Luz.");
 
-	LOG_PRINTLN("GET: Verificar Estado do LED Verde");
+	rest.addData("active", lightState);
+}
 
-	rest.addData("active", statusLedSystemVerde);
+// Route Request
+
+void postRouter(char * params = "")
+{
+
+	LOG_PRINTLN("POST: Ligar ou Desligar a Roteador.");
+
+	if (strncmp(params, "active=0", 8) == 0)
+		routerWith_0();
+
+	else if (strncmp(params, "active=1", 8) == 0)
+		routerWith_1();
+
+	rest.addData("active", routerState);
+}
+
+void getRouter(char * params = "")
+{
+	LOG_PRINTLN("GET: Verificar Estado do Roteador.");
+
+	rest.addData("active", routerState);
+}
+
+// Modem Request
+
+void postModem(char * params = "")
+{
+
+	LOG_PRINTLN("POST: Ligar ou Desligar a Modem.");
+
+	if (strncmp(params, "active=0", 8) == 0)
+		modemWith_0();
+
+	else if (strncmp(params, "active=1", 8) == 0)
+		modemWith_1();
+
+	rest.addData("active", modemState);
+}
+
+void getModem(char * params = "")
+{
+	LOG_PRINTLN("GET: Verificar Estado do Modem.");
+
+	rest.addData("active", modemState);
 }
 
 // Life Cycle
 
 void setup()
 {
-
-	pinMode(ledSystemVerde, OUTPUT);
-
 	LOG_BEGIN(115200);
 
 	LOG_PRINTLN("Iniciando...");
+
+	pinMode(lightPin, OUTPUT);
+	pinMode(routerPin, OUTPUT);
+	pinMode(modemPin, OUTPUT);
 
 	Ethernet.begin(mac, ip);
 
@@ -60,14 +93,85 @@ void setup()
 	LOG_PRINT("IP do Servidor: ");
 	LOG_PRINTLN(Ethernet.localIP());
 
-	// Requests Sets
+	LOG_PRINTLN("Cadastrando Requisições...");
 
-	rest.addRoute(POST, "/ledVerde", postLedStatusVerde);
-	rest.addRoute(GET, "/ledVerde", getLedStatusVerde);
+	// Light Request
+	rest.addRoute(POST, "/light", postLight);
+	rest.addRoute(GET, "/light", getLight);
+
+	// Route Request
+	rest.addRoute(POST, "/router", postRouter);
+	rest.addRoute(GET, "/router", getRouter);
+
+	// Modem Request
+	rest.addRoute(POST, "/modem", postModem);
+	rest.addRoute(GET, "/modem", getModem);
+
+	LOG_PRINTLN("Inicialização Completa.");
 }
 
 void loop()
 {
 
 	rest.run();
+}
+
+// Light
+
+void lightWith_1() 
+{
+	lightState = 1;
+
+	digitalWrite(lightPin, HIGH);
+
+	LOG_PRINTLN("Luz: ON");
+}
+
+void lightWith_0() 
+{
+	lightState = 0;
+
+	digitalWrite(lightPin, LOW);
+
+	LOG_PRINTLN("Luz: OFF");
+}
+
+// Router
+
+void routerWith_1() 
+{
+	routerState = 1;
+
+	digitalWrite(routerPin, HIGH);
+
+	LOG_PRINTLN("Roteador: ON");
+}
+
+void routerWith_0()
+{
+	routerState = 0;
+
+	digitalWrite(routerPin, LOW);
+
+	LOG_PRINTLN("Roteador: OFF");
+}
+
+// Modem
+
+void modemWith_1()
+{
+	modemState = 1;
+
+	digitalWrite(modemPin, HIGH);
+
+	LOG_PRINTLN("Roteador: ON");
+}
+
+void modemWith_0()
+{
+	modemState = 0;
+
+	digitalWrite(modemPin, LOW);
+
+	LOG_PRINTLN("Roteador: OFF");
 }
--- Pinos.h
+++ Pinos.h
@@ -3,25 +3,26 @@
 
 #include <Arduino.h>
 
-// LEDs
+// Light
+int lightPin = 13;
+int lightState = 0;
 
-int ledSystemVerde = 13;
-int statusLedSystemVerde = 0;
+// Router
+int routerPin = 30;
+int routerState = 1;
+
+// Modem
+int modemPin = 31;
+int modemState = 1;
 
 // Ethernet
-
 byte mac[] = { 0x90, 0xA2, 0xDA, 0x10, 0x42, 0xCA };
-
 IPAddress ip(192, 168, 0, 13);
-
 byte gateway[] = { 192, 168, 0, 1 };
-
 byte subnet[] = { 255, 255, 255, 0 };
-
 EthernetServer server(80);
 
 // REST Server
-
 RestServer rest(server);
 
-#endif // ! PINOS
+#endif // !PINOS
